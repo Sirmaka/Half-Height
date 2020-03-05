e71b9ccad92d0b9555c6fb27f35368b32d81370c
Sirmaka <52619139+Sirmaka@users.noreply.github.com>
2020-03-05 18:25:12 +1100

    Worked on Hunter

---
+++ Assets/Scripts/Hunter/HunterCaller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script is to control everything regarding the Hunter. It will call the methods from HunterController.
+
+public class HunterCaller : MonoBehaviour
+{
+    public HunterController Controller;
+    public float interval;
+    private float intervalReset;
+    public bool attacking = false;
+    private int attackOption;
+    public bool canAttack;
+    public float PSwindUp;//Powerswipe windup time
+    // Start is called before the first frame update
+    void Start()
+    {
+        intervalReset = interval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //if not attacking, but can attack, wait until the cooldown ends.
+        if(!attacking && canAttack){
+            interval -= 1;
+        }
+        if(interval <= 0){
+            attacking = true;
+            interval = intervalReset;
+        }
+        if(attacking && canAttack){
+            //call Powerswipe
+            int choice = Random.Range(0,1); //guarantees Powerswipe option;
+            switch(choice){
+                default:
+                    Controller.Powerswipe(PSwindUp);
+                    break;
+            }
+            canAttack = false;
+            //intention: this gets called only once, and only starts again once canAttack = true again;
+        }
+    }
+}
---
+++ Assets/Scripts/Hunter/HunterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterController : MonoBehaviour
+{
+    public Animator Animator;
+    public Animation anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Powerswipe(float windUp){
+        //animation is handled here
+        Animator.SetBool("Powerswipe", true);
+        //freeze animation until windUp = 0;
+        if(windUp > 0){
+            windUp--;
+        }
+
+        
+    }
+}
